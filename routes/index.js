2c1d3b8659cb2809ee3a5d135aeb98863ee911aa
Syafiq <msyafiq.roikhanm@gmail.com>
2024-01-23 06:22:47 +0700

    ➕ Initial Commit

--- routes/index.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-const router = require('express').Router();
-const importRoutes = require('./import.route');
-
-router.get('/', (req, res) => res.send(`${process.env.APP_NAME} App `));
-router.use('/import', importRoutes);
-
-module.exports = router;
